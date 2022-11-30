7d51989aa3ee172928cd4dc530e9b92b47637328
Francois BONNIN <francois.bonnin@ynov.com>
2022-11-30 14:16:19 +0100

    Sequelize Connection planetscale

--- database.js
+++ database.js
@@ -1,9 +1,6 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-});
+const sequelize = new Sequelize('mysql://rkrv5lqrs55qijvyd1yw:pscale_pw_xnVS1Hl0rgIJcteTBsCIAxJjMawpUVcQDTIGaofkXI9@eu-central.connect.psdb.cloud/munch', { dialect: "mysql", dialectOptions: { ssl: {} } });
 
 module.exports = sequelize
