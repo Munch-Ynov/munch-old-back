8af8fe8a49969e66aeee98a9ae58851f9ad14192
Francois BONNIN <francois.bonnin@ynov.com>
2022-11-30 14:21:54 +0100

    env variable

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const sequelize = new Sequelize('mysql://rkrv5lqrs55qijvyd1yw:pscale_pw_xnVS1Hl0rgIJcteTBsCIAxJjMawpUVcQDTIGaofkXI9@eu-central.connect.psdb.cloud/munch', { dialect: "mysql", dialectOptions: { ssl: {} } });
+const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: "mysql", dialectOptions: { ssl: {} } });
 
 module.exports = sequelize
