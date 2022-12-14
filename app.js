6928fb3677868c9ef12be657232da5953a4fd860
Maceo <basse.maceo@gmail.com>
2022-12-14 16:25:15 +0100

    add cors

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./openapi.yaml');
 const OpenApiValidator = require('express-openapi-validator');
-
+const cors = require("cors");
 const restaurantsRoutes = require("./routes/restaurants.router");
 const usersRoutes = require("./routes/users.router");
 
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
